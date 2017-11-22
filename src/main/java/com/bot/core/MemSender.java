e18735c806c52e48166229f511d9e198fb76b409
VictorYezhov <vicyezh@gmail.com>
2017-11-22 09:29:22 +0200

    update

--- src/main/java/com/bot/core/MemSender.java
+++ src/main/java/com/bot/core/MemSender.java
@@ -1,16 +1,13 @@
 package com.bot.core;
-import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.objects.PhotoSize;
-import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.objects.Message;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
+
 
 /**
  * Created by Victor on 21.11.2017.
  */
+
+
 public class MemSender {
 
     public static SendPhoto createMemToSend(Message message, String f_id){
